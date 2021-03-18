9a6f045861584d6d936967323788225094953c0d
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-18 03:40:59 -0400

    added starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'butterrolls#main'
+
+  
+  get 'main', :to => 'butterrolls#main', as: 'main'
+  get 'about', :to => 'butterrolls#about', as: 'about'
+  
+  resources :words
+  resources :phrases
+  resources :matches
+
 end
