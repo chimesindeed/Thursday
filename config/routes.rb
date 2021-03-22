606a1a0719bfa227fc4774b863474c5711e953fe
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-21 23:42:24 -0400

    added route route to welcome#main, also added welcome#about

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'welcome#main'
 
-  
-  get 'main', :to => 'welcome#main', as: 'main'
-  get 'about', :to => 'welcome#about', as: 'about'
+  # get 'about', :to => 'welcome#about', as: 'about'
   
   resources :words
   resources :phrases
