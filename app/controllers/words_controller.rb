5070c32761aeadf7735a0f359ff598056af1b67b
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-21 22:02:12 -0400

    fleshed out Word controller actions including strong params

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,46 @@
 class WordsController < ApplicationController
+	
+	def index
+		@words = Word.all
+	end
+
+	def new
+		@word = Word.new
+	end
+
+	def create
+		@word = Word.new(word_params)
+		if @word.save
+			flash[:notice] = 'Word was successfully saved!'
+			redirect_to_words_path
+		else
+			flash[:notice] = "Word not saved"
+			redirect_to_new_word_path
+		end
+	end
+
+	def show
+		@word = Word.find(params[:id])
+	end
+
+	def edit
+		@word = Word.find(params[:id])
+	end
+
+	def update
+		@word = Word.find(params[:id])
+		@word.update(name: params[:word][:name])
+		redirect_to_words_path
+	end
+
+	def destroy
+		@word = Word.find(params[:id])
+		@word.destroy
+		redirect_to_words_path
+	end
+
+	def word_params
+		params.require(:word).permit(:name)
+	end
+	
 end
